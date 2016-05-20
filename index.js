762894ad5c27cff6b7c985f38219e80a34fbe41a
dxing <domett.xing@gmail.com>
2016-05-20 18:56:13 +1000

    qualification-2016-B

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var inputDataString = fs.readFileSync('smaller.in').toString();
+var inputDataString = fs.readFileSync('large.in').toString();
 var inputData = inputDataString.split("\n");
 
 var testCases = inputData.shift();
@@ -8,35 +8,24 @@ var inputs = inputData;
 var results = [];
 
 for (var n = 0; n < inputs.length; n++) {
-	results.push("Case #" + (n+1) + ": " + sheep(inputs[n]));
+	results.push("Case #" + (n+1) + ": " + pancakes(inputs[n]));
 }
 
 fs.writeFileSync('output.in', results.join("\n"));
 
-function sheep(N) {
-	var number;
-	var counter = 1;
-	var digitsDone = [];
-	while (!test(digitsDone) && counter < 100) {
-		number = N * counter;
-		digitsDone = number.toString().split("").concat(digitsDone);
-		counter++ 
-		}
-	if (counter === 100) {
-		return 'INSOMNIA';
+function pancakes(S) {
+	var counter = 0;
+	var firstPancake;
+	var flippedDirection;
+	var flipNumber;
+	while (S.indexOf('-') > -1) {
+		firstPancake = S.charAt(0);
+		if (firstPancake === '-') flippedDirection = '+';
+		else if (firstPancake === '+') flippedDirection = '-';
+		if (S.indexOf(flippedDirection) === -1) flipNumber = S.length;
+		else flipNumber =  S.indexOf(flippedDirection);
+		S = flippedDirection.repeat(flipNumber).concat(S.slice(flipNumber));
+		counter++;
 	}
-	return number;
-	}	
-
-function test(digitsDone) {
-	var done = [false, false, false, false, false, false, false, false, false, false];
-	for (var i = 0; i < digitsDone.length; i++) {
-		done[digitsDone[i]] = true;
-	}
-	for (var j = 0; j < done.length; j++) {
-		if (!done[j]) {
-			return false;
-		}
-	}
-	return true;
+	return counter;
 }
